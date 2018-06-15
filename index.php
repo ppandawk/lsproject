f850a93b28689709cdd978aa6d4c530c6e0c79c7
P <wongpat.kit@pantavanij.com>
2018-06-15 14:00:04 +0700

    fixed

--- index.php
+++ index.php
@@ -32,13 +32,60 @@
 			</div>
 			<div class="main-container">
 				<div class="box-row pad-20">
-					<div class="column-5">
-						dfdf
+					<div class="column-4">
+						<div class="c-img">
+							<div class="bg-op"></div>
+							<img src="https://dummyimage.com/400x400/e8d492/4e4f63.png">
+						</div>
+						<div class="c-img">
+							<div class="bg-op"></div>
+							<img src="https://dummyimage.com/400x400/e8d492/4e4f63.png">
+						</div>
 					</div>
-					<div class="column-7">
-						dfdf
-					</div>		
+					<div class="column-8">
+						<div class="c-img">
+							<div class="bg-op"></div>
+							<img src="https://dummyimage.com/600x600/000/fff.png">
+						</div>
+					</div>	
 				</div>	
+				<div class="clearfix"></div>	
+			</div>
+			<div class="main-container full content">
+				<div class="ls-wrap pad-20">
+					<div class="column-9">
+						<div class="qat-content">
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+						</div>
+					</div>
+					<div class="column-3">
+						<img src="https://dummyimage.com/500x300/000/fff.png">
+					</div>	
+				</div>	
+				<div class="clearfix"></div>	
+			</div>
+			<div class="main-container">
+				<div class="box-row pad-20">
+					<div class="column-6">
+						<img src="https://dummyimage.com/600x600/000/fff.png">
+					</div>
+					<div class="column-6">
+						<div class="box-table">
+							<div class="box-cell">
+								<div class="box-title">
+									<span>ทำไมต้องเลือกเรา</span>
+								</div>
+								<div class="box-content">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+									<br>
+									<br>
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+								</div>
+							</div>
+						</div>
+					</div>	
+				</div>	
+				<div class="clearfix"></div>	
 			</div>
 		</div>
 		<?php include 'src/footer.html';?>
