eedc27666f2aa1059699fd71825aa24a42247c40
P <wongpat.kit@pantavanij.com>
2018-06-25 11:31:21 +0700

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -19,3 +19,59 @@ function showSlides() {
 	    setTimeout(showSlides, 14000);
     }
 }
+var career = [
+	{
+		id: 1,
+		title: 'test',
+		specialist: ['hello','this','is','a','test'],
+		education: ['this','is','a','education']
+	},
+	{
+		id: 2,
+		title: 'hello',
+		specialist: ['hello','test'],
+		education: ['this','education']
+	},
+	{
+		id: 3,
+		title: 'there',
+		specialist: ['hello','this','is','a','test'],
+		education: ['this','is','a','education']
+	}
+]
+
+// modal
+var body = document.getElementsByTagName("body");
+var mainModal = document.getElementsByClassName("cw-modal");
+function isModal(val,key) {
+	var title = document.getElementsByClassName("title");
+	var special = document.getElementsByClassName("specialist");
+	var educa = document.getElementsByClassName("education");
+	body[0].className = body[0].className == '' ? 'freeze' : '';
+	mainModal[0].style.display = mainModal[0].style.display == '' || mainModal[0].style.display == 'none' ? 'block' : 'none';
+
+	title[0].innerHTML = career[key].title;
+	// clear ul before adding
+	if (special[0].children.length > 0) {
+		while (special[0].firstChild) {
+		    special[0].removeChild(special[0].firstChild);
+		}
+	}
+	if (educa[0].children.length > 0) {
+		while (educa[0].firstChild) {
+		    educa[0].removeChild(educa[0].firstChild);
+		}
+	}
+	// it for special
+	for (var i = 0; i < career[key].specialist.length; i++) {
+		var li = document.createElement('li');
+		li.innerHTML = career[key].specialist[i];
+		special[0].appendChild(li);
+	}
+	// it for education
+	for (var i = 0; i < career[key].education.length; i++) {
+		var li = document.createElement('li');
+		li.innerHTML = career[key].education[i];
+		educa[0].appendChild(li);
+	}
+}
