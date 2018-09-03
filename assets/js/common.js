0672588da8e19c7216ae46cb35374f941906486e
P <wongpat.kit@pantavanij.com>
2018-09-03 08:47:35 +0700

    update content

--- assets/js/common.js
+++ assets/js/common.js
@@ -30,7 +30,6 @@ var career = [
 		year: 2018,
 		resp: 'responsibility',
 		specialist: ['การศึกษา: hello','ประสบการณ์: this','ทักษะ: is','อุปนิสัย: a']
-		// education: ['this','is','a','education']
 	},
 	{
 		id: 2,
@@ -39,7 +38,6 @@ var career = [
 		year: 2018,
 		resp: 'responsibility',
 		specialist: ['การศึกษา: hello','ประสบการณ์: this']
-		// education: ['this','education']
 	},
 	{
 		id: 3,
@@ -47,7 +45,6 @@ var career = [
 		date: '20 ก.ค. - 20 ส.ค.',
 		year: 2018,
 		specialist: ['การศึกษา: hello','ประสบการณ์: this','ทักษะ: is','อุปนิสัย: test']
-		// education: ['this','is','a','education']
 	}
 ]
 
@@ -72,23 +69,12 @@ function isModal(val,key) {
 		    special[0].removeChild(special[0].firstChild);
 		}
 	}
-	// if (educa[0].children.length > 0) {
-	// 	while (educa[0].firstChild) {
-	// 	    educa[0].removeChild(educa[0].firstChild);
-	// 	}
-	// }
 	// it for special
 	for (var i = 0; i < career[key].specialist.length; i++) {
 		var li = document.createElement('li');
 		li.innerHTML = career[key].specialist[i];
 		special[0].appendChild(li);
 	}
-	// it for education
-	// for (var i = 0; i < career[key].education.length; i++) {
-	// 	var li = document.createElement('li');
-	// 	li.innerHTML = career[key].education[i];
-	// 	educa[0].appendChild(li);
-	// }
 }
 
 // open sub menu
@@ -126,7 +112,8 @@ var productItem = [
 		code: '0acs3',
 		src: ['assets/images/products/1.png','assets/images/products/2.png','assets/images/products/3.png'],
 		plasticType: ['pet','pvc','pp','ps'],
-		type: 'agi'
+		type: 'agi',
+		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
 	},
 	// ,
 	// {
@@ -232,9 +219,52 @@ function createListProduct(val) {
 	var divImg = "<img src='" + val.src[0] + "'>";
 	var divBoxCell = "<div class='box-cell'>" + divImg + "</div>";
 	var divLpImg = "<div class='lp-img box-table'>" + divBoxCell + "</div><div class='lp-img-list'>" + totalList + "<div class='clearfix'></div></div><div class='lp-info'><div class='li-left'>" + divInfo + divSubInfo + "</div><div class='li-right'>" + totalProductType + "</div></div>";
-	var divLpItem = "<div class='lp-item shadow'>" + divLpImg +"</div>";
-
+	var divLpItem = "<div class='lp-item shadow' onclick='modalProduct(" + JSON.stringify(val) + ")'>" + divLpImg +"</div>";
+	// debugger;
 	$(".list-product").append(divLpItem);
+}
+
+function modalProduct(val,key) {
+	var pName = document.getElementsByClassName("name");
+	var pCode = document.getElementsByClassName("code");
+	var pDesc = document.getElementsByClassName("description");
+
+	body[0].className = body[0].className == '' ? 'freeze' : '';
+	mainModal[0].style.display = mainModal[0].style.display == '' || mainModal[0].style.display == 'none' ? 'block' : 'none';
+
+	pName[0].innerHTML = val.name;
+	pCode[0].innerHTML = val.code;
+	pDesc[0].innerHTML = val.description;
+	
+	var cwMainImg,cwListImg = "";
+
+	if (val) {
+		cwMainImg = "<div class='box-table'><div class='box-cell'><img src='" + val.src[0] + "'></div></div>"
+		$(".ci-wrap").append(cwMainImg);
+
+		for (var a = 0; a < val.src.length; a++) {
+			var divLLimg  = "<img src='" + val.src[a] + "'>";
+			var divLLCell = "<div class='box-cell'>" + divLLimg + "</div>";
+			var divLLitem = "<div class='box-table'>" + divLLCell + "</div>";
+			cwListImg += divLLitem;
+		}
+		$(".ci-wrap-list").append(cwListImg);
+	}else{
+		// clear img
+		var cw = $(".ci-wrap")[0];
+		var cwl = $(".ci-wrap-list")[0];
+		if (cw.children.length > 0) {
+			while (cw.firstChild) {
+			    cw.removeChild(cw.firstChild);
+			}
+		}
+		if (cwl.children.length > 0) {
+			while (cwl.firstChild) {
+			    cwl.removeChild(cwl.firstChild);
+			}
+		}
+	}
+
 }
 
 function tabSelected(val,val2) {
