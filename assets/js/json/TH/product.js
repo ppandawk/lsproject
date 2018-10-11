655b56d06258cff746c423910a7a7b504abd1bc7
P <wongpat.kit@pantavanij.com>
2018-10-11 17:07:53 +0700

    Fixed

--- assets/js/json/TH/product.js
+++ assets/js/json/TH/product.js
@@ -1,3 +1,9 @@
+// Type of product
+// food = อาหาร
+// cos = เครื่องสำอางค์
+// ele = เครื่องใช้ไฟฟ้า
+// agi = การเกษตร
+// haw = อุปกรณ์ก่อสร้าง
 
 var productItem = [
 	{
@@ -6,7 +12,7 @@ var productItem = [
 		code: '0acs3',
 		src: ['cosmetic/bb/1.jpg','cosmetic/bb/2.jpg','cosmetic/bb/3.jpg'],
 		plasticType: ['pet','pvc','pp','ps'],
-		type: 'agi',
+		type: 'cos',
 		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
 	},
 	{
@@ -15,7 +21,7 @@ var productItem = [
 		code: '0acs35555',
 		src: ['cosmetic/natriv_bb/1.jpg','cosmetic/natriv_bb/2.jpg','cosmetic/natriv_bb/3.jpg','cosmetic/natriv_bb/4.jpg'],
 		plasticType: ['pet','pvc','pp','ps'],
-		type: 'agi',
+		type: 'cos',
 		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
 	},
 	{
